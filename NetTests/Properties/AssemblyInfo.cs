20a121f3d218d6e985edf79cce8a6a423b1aef8f
justin <justinamiller@gmail.com>
2019-06-12 15:36:31 -0700

    ADD SOME unit testing

---
+++ NetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a064381-f9e1-4b56-8702-715fe8c5200f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NetTests/SerializeTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTests.TestObjects;
+using Zippy;
+
+namespace NetTests
+{
+    [TestClass]
+    public class SerializeTests
+    {
+        private LargeObject _largeObject = new LargeObject();
+
+        [TestMethod]
+        public void TestNull()
+        {
+            Assert.AreEqual(Zippy.JSON.SerializeObject(null, new StringWriter()), null);
+            Assert.AreEqual(Zippy.JSON.SerializeObjectToString(null), null);
+        }
+
+        [TestMethod]
+        public void TestSerializeObjectComplexObject()
+        {
+            var sw = new StringWriter();
+            Zippy.JSON.SerializeObject(_largeObject, sw);
+            Assert.IsTrue(sw.ToString().Length > 0);
+        }
+
+        [TestMethod]
+        public void TestSerializeObjectToStringComplexObject()
+        {
+            Assert.IsTrue(Zippy.JSON.SerializeObjectToString(_largeObject).Length > 0);
+        }
+    }
+}
